6d982ac65421d70f3cdb5055f995b8369ce6f59f
patelved05 <patelved0512@gmail.com>
2026-07-03 14:19:15 +0530

    Time: 1 ms (55.74%), Space: 312 MB (83.24%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -8,49 +8,82 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+
+//  THIS CODE TAKE FROM GFG AND EDIT SMALL CHANGE 
+// class Solution {
     
-    ListNode* delPos(ListNode* head, int x) {
-        if (head == nullptr)
-            return nullptr;
-        if (x == 1) {
-            ListNode* newHead = head->next;
-            delete head;
-            return newHead;
-        }
+//     ListNode* delPos(ListNode* head, int x) {
+//         if (head == nullptr)
+//             return nullptr;
+//         if (x == 1) {
+//             ListNode* newHead = head->next;
+//             delete head;
+//             return newHead;
+//         }
         
-        ListNode* prevs = head;
-        int count = 1;
+//         ListNode* prevs = head;
+//         int count = 1;
         
-        for (ListNode* curr = head->next; curr != nullptr; curr = curr->next) {
-            count++;
-            if (count == x) {
-                prevs->next = curr->next;
-                delete curr;
-                break;
-            }
-            prevs = curr;
-        }
+//         for (ListNode* curr = head->next; curr != nullptr; curr = curr->next) {
+//             count++;
+//             if (count == x) {
+//                 prevs->next = curr->next;
+//                 delete curr;
+//                 break;
+//             }
+//             prevs = curr;
+//         }
         
-        return head;
-    }	
+//         return head;
+//     }	
     
 
-public:
-    ListNode* deleteMiddle(ListNode* head) {
-        if (head == nullptr || head->next == nullptr) 
-            return nullptr;
+// public:
+//     ListNode* deleteMiddle(ListNode* head) {
+//         if (head == nullptr || head->next == nullptr) 
+//             return nullptr;
         
   
-        int length = 0;
-        ListNode* temp = head;
-        while (temp != nullptr) {
-            length++;
-            temp = temp->next;
+//         int length = 0;
+//         ListNode* temp = head;
+//         while (temp != nullptr) {
+//             length++;
+//             temp = temp->next;
+//         }
+        
+//         int mid = (length / 2) + 1;  
+        
+//         return delPos(head, mid);
+//     }
+// };
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if (head->next == nullptr)
+        {
+            return NULL;
+        }
+        else if (head->next->next == nullptr)
+        {
+            ListNode* temp = head->next;
+            head->next = nullptr;
+            delete temp;
+            return head;
+        }
+        else
+        {
+            ListNode* fp = head;
+            ListNode* sp = head;
+            while (sp->next->next != nullptr && sp->next->next->next != nullptr)
+            {
+                sp = sp->next->next;
+                fp = fp->next;
+            }
+            ListNode* temp = fp->next;
+            fp->next = fp->next->next;
+            delete temp;
+            return head;
         }
         
-        int mid = (length / 2) + 1;  
-        
-        return delPos(head, mid);
     }
 };
