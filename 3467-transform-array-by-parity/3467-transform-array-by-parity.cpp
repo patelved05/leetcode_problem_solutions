4c70474ab2bbedf037bf463f0121b5a627393624
patelved05 <patelved0512@gmail.com>
2026-07-14 22:19:34 +0530

    Time: 0 ms (100%), Space: 28.2 MB (89.56%) - LeetHub

--- 3467-transform-array-by-parity/3467-transform-array-by-parity.cpp
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.cpp
@@ -1,3 +1,28 @@
+// class Solution {
+// public:
+//     vector<int> transformArray(vector<int>& nums) {
+//         int zeroCount=0;
+//         int oneCount=0;
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]&1)
+//             oneCount++;
+//             else
+//             zeroCount++;
+//         }
+
+//         for(int i=0;i<nums.size();i++){
+//             if(zeroCount!=0){
+//                 nums[i]=0;
+//                 zeroCount--;
+//                 continue;
+//             }
+//             else{
+//                 nums[i]=1;
+//             }
+//         }
+//         return nums;
+//     }
+// };
 class Solution {
 public:
     vector<int> transformArray(vector<int>& nums) {
